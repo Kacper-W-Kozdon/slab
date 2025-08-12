f4db67cac30fc2f2282218badebe41e673f1a4b5
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-08-12 18:05:11 +0200

    adds: A_param for n >= 1

--- Contini.py
+++ Contini.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from typing import Optional, Union
 
-from numpy import exp, pi
+from numpy import exp, log, pi
 
 
 def Contini(
@@ -15,6 +15,7 @@ def Contini(
     phantom: Optional[str],
     DD: Optional[str],
     m: int = 200,
+    eq: str = "RTE",
 ):
     t = t * 1e-9
     rho = rho * 1e-3
@@ -25,35 +26,35 @@ def Contini(
     err = 1e-6  # noqa: F841
 
     R_rho_t, T_rho_t = Reflectance_Transmittance_rho_t(
-        rho, t, s, m, mua, musp, n1, n2, DD
+        rho, t, s, m, mua, musp, n1, n2, DD, eq
     )
 
-    R_rho, T_rho = Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD)
+    R_rho, T_rho = Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD, eq)
 
-    R_t, T_t = Reflectance_Transmittance_t(t, s, m, mua, musp, n1, n2, DD)
+    R_t, T_t = Reflectance_Transmittance_t(t, s, m, mua, musp, n1, n2, DD, eq)
 
-    l_rho_R, l_rho_T = Mean_Path_T_R(rho, s, m, mua, musp, n1, n2, DD)
+    l_rho_R, l_rho_T = Mean_Path_T_R(rho, s, m, mua, musp, n1, n2, DD, eq)
 
-    R, T = Reflectance_Transmittance(s, m, mua, musp, n1, n2, DD)
+    R, T = Reflectance_Transmittance(s, m, mua, musp, n1, n2, DD, eq)
 
     A = A_param(n1, n2)
 
-    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m)
+    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq)
 
     return R_rho_t, T_rho_t, R_rho, T_rho, R_t, T_t, l_rho_R, l_rho_T, R, T, A, Z
 
 
-def D_parameter(DD, mua, musp):
+def D_parameter(DD, mua, musp, eq):
     D = None
 
     return D
 
 
-def Reflectance_Transmittance_rho_t(rho, t, s, m, mua, musp, n1, n2, DD):
+def Reflectance_Transmittance_rho_t(rho, t, s, m, mua, musp, n1, n2, DD, eq):
     c = 299792458
     v = c / n2
 
-    D = D_parameter(DD, mua, musp)
+    D = D_parameter(DD, mua, musp, eq)
 
     R_rho_t = 0.0
     T_rho_t = 0.0
@@ -61,7 +62,7 @@ def Reflectance_Transmittance_rho_t(rho, t, s, m, mua, musp, n1, n2, DD):
     R_rho_t_source_sum = 0.0
     T_rho_t_source_sum = 0.0
 
-    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m)
+    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq)
 
     for index in range(-m, m + 1):
         z1, z2, z3, z4 = Z[f"Z_{index}"]
@@ -94,15 +95,15 @@ def Reflectance_Transmittance_rho_t(rho, t, s, m, mua, musp, n1, n2, DD):
     return R_rho_t, T_rho_t
 
 
-def Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD):
+def Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD, eq):
     R_rho, T_rho = None, None
 
-    D = D_parameter(DD, mua, musp)
+    D = D_parameter(DD, mua, musp, eq)
 
     R_rho_source_sum = 0
     T_rho_source_sum = 0
 
-    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m)
+    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq)
 
     for index in range(-m, m + 1):
         z1, z2, z3, z4 = Z[f"Z_{index}"]
@@ -111,8 +112,8 @@ def Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD):
             D ** (-1 / 2) * mua ** (1 / 2) * (rho**2 + z3**2) ** (-1)
             + (rho**2 + z3**2) ** (-3 / 2)
         ) * exp(-((mua * (rho**2 + z3**2) / D) ** (1 / 2))) - z4 * (
-            D ** (-1 / 2) * mua
-            ^ (1 / 2) * (rho**2 + z4**2) ** (-1) + (rho**2 + z4**2) ** (-3 / 2)
+            D ** (-1 / 2) * mua ** (1 / 2) * (rho**2 + z4**2) ** (-1)
+            + (rho**2 + z4**2) ** (-3 / 2)
         ) * exp(-((mua * (rho**2 + z4**2) / D) ** (1 / 2)))
 
         if m == 0:
@@ -122,8 +123,8 @@ def Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD):
                 D ** (-1 / 2) * mua ** (1 / 2) * (rho**2 + z1**2) ** (-1)
                 + (rho**2 + z1**2) ** (-3 / 2)
             ) * exp(-((mua * (rho**2 + z1**2) / D) ** (1 / 2))) - z2 * (
-                D ** (-1 / 2) * mua
-                ^ (1 / 2) * (rho**2 + z2**2) ** (-1) + (rho**2 + z2**2) ** (-3 / 2)
+                D ** (-1 / 2) * mua ** (1 / 2) * (rho**2 + z2**2) ** (-1)
+                + (rho**2 + z2**2) ** (-3 / 2)
             ) * exp(-((mua * (rho**2 + z2**2) / D) ** (1 / 2)))
 
     R_rho = -1 / (4 * pi) * R_rho_source_sum
@@ -135,17 +136,17 @@ def Reflectance_Transmittance_rho(rho, s, m, mua, musp, n1, n2, DD):
     return R_rho, T_rho
 
 
-def Reflectance_Transmittance_t(t, s, m, mua, musp, n1, n2, DD):
+def Reflectance_Transmittance_t(t, s, m, mua, musp, n1, n2, DD, eq):
     R_t, T_t = None, None
 
     c = 299792458
     v = c / n2
-    D = D_parameter(DD, mua, musp)
+    D = D_parameter(DD, mua, musp, eq)
 
     R_t_source_sum = 0
     T_t_source_sum = 0
 
-    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m)
+    Z = Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq)
 
     for index in range(-m, m + 1):
         z1, z2, z3, z4 = Z[f"Z_{index}"]
@@ -174,12 +175,12 @@ def Mean_Path_T_R(rho, s, m, mua, musp, n1, n2, DD):
     return l_rho_R, l_rho_T
 
 
-def Reflectance_Transmittance(s, m, mua, musp, n1, n2, DD):
+def Reflectance_Transmittance(s, m, mua, musp, n1, n2, DD, eq):
     R, T = None, None
     return R, T
 
 
-def A_param(n1, n2):
+def A_param_approx(n1, n2):
     A = None
     A = (
         504.332889
@@ -194,9 +195,106 @@ def A_param(n1, n2):
     return A
 
 
-def Image_Sources_Positions(s, mua, musp, n1, n2, DD, m):
+def A_param(n1, n2):
+    A = 0
+    n = n2 / n1
+
+    if n == 1:
+        A = 1
+        return A
+
+    if n > 1:
+        t1 = (
+            4
+            * (
+                -1
+                - n**2
+                + 6 * n**3
+                - 10 * n**4
+                - 3 * n**5
+                + 2 * n**6
+                + 6 * n**7
+                - 3 * n**8
+                - (6 * n**2 + 9 * n**6) * (n**2 - 1) ** (1 / 2)
+            )
+            / (3 * n * (n**2 - 1) ** 2 * (n**2 + 1) ** 3)
+        )
+        t2 = (
+            -8
+            + 28 * n**2
+            + 35 * n**3
+            - 140 * n**4
+            + 98 * n**5
+            - 13 * n**7
+            + 13 * n * (n**2 - 1) ** 3 * (1 - (1 / n**2)) ** (1 / 2)
+        ) / (105 * n**3 * (n**2 - 1) ** 2)
+        t3 = (
+            2
+            * n**3
+            * (3 + 2 * n**4)
+            * log(
+                (
+                    (n - (1 + n**2) ** (1 / 2))
+                    * (2 + n**2 + 2 * (1 + n**2) ** (1 / 2))
+                    * (n**2 + (n**4 - 1) ** (1 / 2))
+                )
+                / (
+                    n**2
+                    * (n + (1 + n**2) ** (1 / 2))
+                    * (-(n**2) + (n**4 - 1) ** (1 / 2))
+                )
+            )
+            / ((n**2 - 1) ** 2 * (n**2 + 1) ** (7 / 2))
+        )
+        t4 = (
+            (1 + 6 * n**4 + n**8) * log((-1 + n) / (1 + n))
+            + 4 * (n**2 + n**6) * log((n**2 * (1 + n)) / (n - 1))
+        ) / ((n**2 - 1) ** 2 * (n**2 + 1) ** 3)
+
+        B = 1 + (3 / 2) * (
+            2 * (1 - 1 / n**2) ** (3 / 2) / 3
+            + t1
+            + t2
+            + ((1 + 6 * n**4 + n**8) * (1 - (n**2 - 1) ** (3 / 2) / n**3))
+            / (3 * (n**4 - 1) ** 2)
+            + t3
+        )
+        C = (
+            1
+            - (
+                (
+                    2
+                    + 2 * n
+                    - 3 * n**2
+                    + 7 * n**3
+                    - 15 * n**4
+                    - 19 * n**5
+                    - 7 * n**6
+                    + 3 * n**7
+                    + 3 * n**8
+                    + 3 * n**9
+                )
+                / (3 * n**2 * (n - 1) * (n + 1) ** 2 * (n**2 + 1) ** 2)
+            )
+            - t4
+        )
+        A = B / C
+
+        return A
+
+    return A
+
+
+def Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq):
     Z = OrderedDict()
+
+    z0 = 1 / musp  # noqa: F841
+    # z0 = 1 / (mua + musp)
+
+    A = A_param(n1, n2)  # noqa: F841
+
     for index in range(-m, m + 1):
         z1, z2, z3, z4 = None, None, None, None
+
         Z[f"Z_{index}"] = z1, z2, z3, z4
     return Z
