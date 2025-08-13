fix: R_rho_t and T_rho_t calculation

main.py
@@ -23,17 +23,17 @@ if __name__ == "__main__":
 
     rng = np.random.default_rng()
     noise = 0.0000005 * rng.normal(size=len(xdata))
-    print(noise)
+    # print(noise)
 
     for index in range(len(ydata)):
         ydata_noisy.append(ydata[index] + noise[index])
 
-    print(ydata)
+    # print(ydata)
 
     contini.mua = 0
     contini.musp = None
 
-    popt, pcov = curve_fit(contini.fit, xdata, ydata_noisy, [0.95], bounds=(0.001, 2.0))
+    popt, pcov = curve_fit(contini.fit, xdata, ydata_noisy, [1], bounds=(0.001, 2.0))
 
     print(popt)
     print(pcov)
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     for coord in xdata:
         xdata_t.append(coord[0])
 
-    print(xdata_t)
+    # print(xdata_t)
     plot1 = plt.plot(xdata_t, ydata, color="r", label="data1")
 
     contini2 = Contini(s=40, musp=popt[0], n1=1, n2=1)