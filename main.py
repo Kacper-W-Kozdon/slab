fix: R_rho_t and T_rho_t calculation

main.py
@@ -1,4 +1,8 @@
 # placeholder
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
 from modules import Contini
 
 if __name__ == "__main__":
@@ -6,12 +10,53 @@ if __name__ == "__main__":
 
     rho = 1
 
-    result = []
+    ydata = []
+    xdata = []
+    ydata_noisy = []
 
-    for t_index, t in enumerate(range(5, 105, 5)):
-        picot = 0.001 * t
+    for t_index, t in enumerate(range(10, 510, 10)):
+        picot = t * 1e-3
         subresult = contini((picot, rho))
 
-        result.append(tuple([subresult[0], subresult[1]]))
+        ydata.append(subresult[0])
+        xdata.append(tuple([picot, rho]))
 
-    print(result)
+    rng = np.random.default_rng()
+    noise = 0.0000005 * rng.normal(size=len(xdata))
+    print(noise)
+
+    for index in range(len(ydata)):
+        ydata_noisy.append(ydata[index] + noise[index])
+
+    print(ydata)
+
+    contini.mua = 0
+    contini.musp = None
+
+    popt, pcov = curve_fit(contini.fit, xdata, ydata_noisy, [1], bounds=(0.001, 2.0))
+
+    print(popt)
+    print(pcov)
+
+    xdata_t = []
+    for coord in xdata:
+        xdata_t.append(coord[0])
+
+    print(xdata_t)
+    plot1 = plt.plot(xdata_t, ydata_noisy, color="r", label="data1")
+
+    contini2 = Contini(s=40, musp=popt[0], n1=1, n2=1)
+
+    ydata = []
+    xdata = []
+    ydata_noisy = []
+
+    for t_index, t in enumerate(range(10, 510, 10)):
+        picot = t * 1e-3
+        subresult = contini2((picot, rho))
+
+        ydata.append(subresult[0])
+        xdata.append(tuple([picot, rho]))
+
+    plot2 = plt.plot(xdata_t, ydata, color="g", label="data2")
+    plt.show()