20271ac27c86480c04f30cc03c7b5d0e8c4ba6fe
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-08-14 02:37:07 +0200

    edit: example from the main file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from modules import Contini
 
 if __name__ == "__main__":
-    contini = Contini(s=40, mua=0.05, musp=0.5, n1=1, n2=1)
+    contini = Contini(s=40, mua=0.05, musp=0.05, n1=1, n2=1)
 
     rho = 1
 
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     xdata = []
     ydata_noisy = []
 
-    for t_index, t in enumerate(range(10, 2010, 10)):
-        picot = t * 1e-5
+    for t_index, t in enumerate(range(1, 201, 1)):
+        picot = t * 1e-3
         subresult = contini((picot, rho))
 
         ydata.append(subresult[0])
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     contini.musp = None
 
     # popt, pcov = curve_fit(contini._fit, xdata, ydata_noisy, [0.9])
-    popt, pcov = contini.fit(xdata, ydata_noisy, [0.4])
+    popt, pcov = contini.fit(xdata, ydata_noisy, [0.04])
 
     print(popt)
     print(pcov)
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         xdata_t.append(coord[0])
 
     # print(xdata_t)
-    plot1 = plt.plot(xdata_t, ydata_noisy, color="r", label="data1")
+    plot1 = plt.plot(xdata_t, np.log(ydata_noisy), color="r", label="data1")
 
     contini2 = Contini(s=40, musp=popt[0], n1=1, n2=1)
 
@@ -51,12 +51,12 @@ if __name__ == "__main__":
     xdata = []
     ydata_noisy = []
 
-    for t_index, t in enumerate(range(10, 2010, 10)):
-        picot = t * 1e-5
+    for t_index, t in enumerate(range(1, 201, 1)):
+        picot = t * 1e-3
         subresult = contini2((picot, rho))
 
         ydata.append(subresult[0])
         xdata.append(tuple([picot, rho]))
 
-    plot2 = plt.plot(xdata_t, ydata, color="g", label="data2")
+    plot2 = plt.plot(xdata_t, np.log(ydata), color="g", label="data2")
     plt.show()
