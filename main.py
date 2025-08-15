a30488538c24e3be84dbffa5dd10b9036f06347f
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-08-15 17:20:21 +0200

    updates: main.py example

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import pathlib
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from modules import Contini
 
@@ -15,57 +14,70 @@ if __name__ == "__main__":
     xdata = []
     ydata_noisy = []
 
-    for t_index, t in enumerate(range(1, 311, 2)):
-        picot = t
-        subresult = contini((picot, rho))
+    # for t_index, t in enumerate(range(1, 311, 2)):
+    #     picot = t
+    #     subresult = contini((picot, rho))
 
-        ydata.append(subresult[0])
-        xdata.append(tuple([picot, rho]))
+    #     ydata.append(subresult[0])
+    #     xdata.append(tuple([picot, rho]))
 
-    rng = np.random.default_rng()
-    noise = rng.normal(size=len(xdata))
-    # print(noise)
+    # rng = np.random.default_rng()
+    # noise = rng.normal(size=len(xdata))
+    # # print(noise)
 
-    for index in range(len(ydata)):
-        ydata_noisy.append(ydata[index] + 0.05 * ydata[index] * noise[index])
+    # for index in range(len(ydata)):
+    #     ydata_noisy.append(ydata[index] + 0.05 * ydata[index] * noise[index])
 
-    # print(ydata)
-    print(ydata_noisy)
-    contini.mua = 0.05
-    contini.musp = None
+    # # print(ydata)
+    # print(ydata_noisy)
+    # contini.mua = 0.05
+    # contini.musp = None
 
-    # popt, pcov = curve_fit(contini._fit, xdata, ydata_noisy, [0.9])
-    popt, pcov = contini.fit(xdata, ydata_noisy, [0.25])
+    # # popt, pcov = curve_fit(contini._fit, xdata, ydata_noisy, [0.9])
+    # popt, pcov = contini.fit(xdata, ydata_noisy, [0.25])
 
-    print(popt)
-    print(pcov)
+    # print(popt)
+    # print(pcov)
 
-    xdata_t = []
-    for coord in xdata:
-        xdata_t.append(coord[0])
+    # xdata_t = []
+    # for coord in xdata:
+    #     xdata_t.append(coord[0])
 
-    # print(xdata_t)
-    plot1 = plt.plot(xdata_t, ydata_noisy, color="r", label="noisy")
-    plot0 = plt.plot(xdata_t, ydata, color="b", label="control")
+    # # print(xdata_t)
+    # plot1 = plt.plot(xdata_t, ydata_noisy, color="r", label="noisy")
+    # plot0 = plt.plot(xdata_t, ydata, color="b", label="control")
 
-    contini2 = Contini(s=40, musp=popt[0], n1=1, n2=1)
+    # contini2 = Contini(s=40, musp=popt[0], n1=1, n2=1)
 
-    ydata = []
-    xdata = []
-    ydata_noisy = []
+    # ydata = []
+    # xdata = []
+    # ydata_noisy = []
 
-    for t_index, t in enumerate(range(1, 311, 2)):
-        picot = t
-        subresult = contini2((picot, rho))
+    # for t_index, t in enumerate(range(1, 311, 2)):
+    #     picot = t
+    #     subresult = contini2((picot, rho))
 
-        ydata.append(subresult[0])
-        xdata.append(tuple([picot, rho]))
+    #     ydata.append(subresult[0])
+    #     xdata.append(tuple([picot, rho]))
 
-    plot2 = plt.plot(xdata_t, ydata, color="g", label="fit")
+    # plot2 = plt.plot(xdata_t, ydata, color="g", label="fit")
     # plt.yscale("log")
-    plt.show()
 
     path = f"{pathlib.Path(__file__).parent.resolve()}\\test_data\\all_raw_data_combined.xlsx"
     if pathlib.Path(path).exists():
         df = pd.read_excel(path, engine="openpyxl")
         print(df.head())
+
+        df = pd.read_excel(path, engine="openpyxl")
+        df_time = df.iloc[:, 0]
+        df_ydata = df.iloc[:, 1]
+
+        raw_data = plt.plot(
+            df_time, df_ydata, color="b", label="raw data", marker="o", linestyle=" "
+        )
+
+        xdata = [tuple([time, rho]) for time in df_time]
+        popt, pcov = contini.fit(xdata, df_ydata, [0.25])
+        print(popt, pcov)
+
+        plt.show()
