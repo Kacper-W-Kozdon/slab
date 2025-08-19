04926a8709d8df18abd99bd38bcf2ce3f8fa87ea
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-08-19 21:33:09 +0200

    edit: main example

--- main.py
+++ main.py
@@ -106,11 +106,11 @@ if __name__ == "__main__":
     path = f"{pathlib.Path(__file__).parent.resolve()}\\dataset\\all_raw_data_combined.xlsx"
     if pathlib.Path(path).exists():
         initial_params = {
-            "mua": 0.065,
+            "mua": 0.05,
             "musp": 0.05,
             "offset": 0,
             "lower_bounds": [0.01, 0.01, 0.0],
-            "upper_bounds": [0.1, 0.1, 50],
+            "upper_bounds": [0.6, 0.6, 50],
         }
 
         rho = 5
