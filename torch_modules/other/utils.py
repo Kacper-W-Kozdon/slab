fix: t_rho in tContini had incorrect shape- fixed by adding transposition

torch_modules/other/utils.py
@@ -2,9 +2,8 @@ import warnings
 from collections import OrderedDict
 from decimal import localcontext
 
-import numpy as np
 import torch
-from numpy import exp, log, sqrt
+from numpy import log
 from scipy.special import factorial, gamma
 
 
@@ -209,15 +208,16 @@ def Image_Sources_Positions(s, mua, musp, n1, n2, DD, m, eq):
 def G_func(input, N_scatter=200, mode: str = "correction", **kwargs):
     G = torch.full_like(input, 0.0)
 
-    x = torch.where(~torch.isnan(input), input, 0.0)
+    # x = torch.where(~torch.isnan(input), input, 0.0)
+    x = input.clone().detach().requires_grad_(True)
 
     if mode == "sum":
         with localcontext() as ctx:
             ctx.prec = 100
-            unfiltered_factor = 8 * (3 * x) ** (-3 / 2)
-            factor = torch.where(
-                ~torch.isnan(unfiltered_factor), unfiltered_factor, 0.0
-            )
+            factor = 8 * (3 * x) ** (-3 / 2)
+            # factor = torch.where(
+            #     ~torch.isnan(unfiltered_factor), unfiltered_factor, 0.0
+            # )
 
             for N in range(1, N_scatter + 1):
                 try:
@@ -237,20 +237,21 @@ def G_func(input, N_scatter=200, mode: str = "correction", **kwargs):
         ret = torch.where(~torch.isnan(G) * ~torch.isinf(G), G, 0.0)
         return ret
     if mode == "approx":
-        G += exp(x) * sqrt(1 + 2.026 / x)
+        G += torch.exp(x) * torch.sqrt(1 + 2.026 / x)
 
         ret = torch.where(~torch.isnan(G) * ~torch.isinf(G), G, 0.0)
+        # print(f"{x=}")
         return ret
 
     if mode == "correction":
         with warnings.catch_warnings():  # stop warnings about negative value under sqrt, we don't use that region
             warnings.simplefilter("ignore")
-            unfiltered_cor_factor = cor_factor(
+            correction_factor = cor_factor(
                 x, 1.19318303, 1.41879319, 4.98107131, 5.54541984
             )
-            correction_factor = torch.where(
-                ~torch.isnan(unfiltered_cor_factor), unfiltered_cor_factor, 0.0
-            )
+            # correction_factor = torch.where(
+            #     ~torch.isnan(unfiltered_cor_factor), unfiltered_cor_factor, 0.0
+            # )
             G = (
                 G_func(x, N_scatter=N_scatter, mode="sum", **kwargs) * (x <= 80)
                 + G_func(x, N_scatter=N_scatter, mode="approx", **kwargs)
@@ -269,5 +270,5 @@ def G_func(input, N_scatter=200, mode: str = "correction", **kwargs):
 
 
 def cor_factor(x, a, b, x_0, c):
-    r = a * np.sqrt(b * (x - x_0) + c)
+    r = a * torch.sqrt(b * (x - x_0) + c)
     return r

torch_modules/reflect_transmit/reflect_transmit.py
@@ -143,18 +143,19 @@ def Reflectance_Transmittance_rho_t(
             Delta_plus = 1.0 * torch.tensor(r_plus == c * t)
             Delta_minus = 1.0 * torch.tensor(r_minus == c * t)
             Theta_plus = 1.0 * torch.tensor(r_plus < c * t)
-            Theta_minus = 1 * torch.tensor(r_minus < c * t)
+            Theta_minus = 1.0 * torch.tensor(r_minus < c * t)
             # print(f"{r_plus=}")
-            # print(f"{Delta_plus=}")
+            # print(f"{Theta_plus=}")
             G_plus = Theta_plus * G_func(
                 c * t / mean_free_path * (1 - r_plus**2 / (c**2 * t**2)) ** (3 / 4),
                 **kwargs,
             )
+            print(f"{(1 - r_plus**2 / (c**2 * t**2)) ** (3 / 4)=}, {t=}")
             G_minus = Theta_minus * G_func(
                 c * t / mean_free_path * (1 - r_minus**2 / (c**2 * t**2)) ** (3 / 4),
                 **kwargs,
             )
-
+            print(f"{all(G_plus == 0.)=}, {all(Theta_plus == 0.)=}")
             factor_plus_unfiltered = Theta_plus * (1 - r_plus**2 / (c**2 * t**2)) ** (
                 1 / 8
             )
@@ -189,6 +190,8 @@ def Reflectance_Transmittance_rho_t(
 
         R_rho_t = 1 / (2 * A) * R_rho_t_source_sum
         T_rho_t = 1 / (2 * A) * T_rho_t_source_sum
+        if all(T_rho_t == 0.0) or all(R_rho_t == 0.0):
+            raise ValueError(f"Found only 0. values in the output. {T_rho_t=}")
         # R_rho_t = R_rho_t if R_rho_t > 0 else -R_rho_t
         # T_rho_t = T_rho_t if T_rho_t > 0 else -T_rho_t
 

torch_modules/t_method_classes/t_Contini_Class.py
@@ -255,7 +255,8 @@ class tContini(Module, BaseClass):
         """
         if not isinstance(t_rho, torch.Tensor):
             try:
-                t_rho = torch.tensor(t_rho).reshape(2, -1)
+                t_rho = torch.tensor(t_rho)
+
             except Exception as exc:
                 print(
                     f"There was an issue converting t_rho to torch.tensor.\n\n\
@@ -265,6 +266,7 @@ class tContini(Module, BaseClass):
             # mua = mua * 1e3 if self._mua is None else self._mua
             # musp = musp * 1e3 if self._musp is None else self._musp
 
+        t_rho_transposed = t_rho.clone().detach().mT.reshape(2, -1).requires_grad_(True)
         if mua is None:
             mua = self._mua
         else:
@@ -276,8 +278,8 @@ class tContini(Module, BaseClass):
 
         anisothropy_coeff = anisothropy_coeff or self.controls.get("anisothropy_coeff")
 
-        t = t_rho[0] * 1e-12
-        rho = t_rho[1] * 1e-3
+        t = t_rho_transposed[0] * 1e-12
+        rho = t_rho_transposed[1] * 1e-3
         s = kwargs.get("s") or self.controls.get("_s")
         n1 = kwargs.get("n1") or self.controls.get("n1")
         n2 = kwargs.get("n2") or self.controls.get("n2")